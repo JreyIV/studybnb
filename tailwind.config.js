172338681507ece72ef5681f02a70e73cf86b829
Joseph Reyes <jreyes1919@gmail.com>
2024-04-02 18:13:04 -0500

    installed tailwind, daisyui, and framer motion

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "19.5px"],
+      lg: ["18px", "21.94px"],
+      xl: ["20px", "24.38px"],
+      "2xl": ["24px", "29.26px"],
+      "3xl": ["28px", "50px"],
+      "4xl": ["48px", "58px"],
+      "6xl": ["64px", "80px"],
+      "8xl": ["96px", "106px"],
+      Mammoth: ["120px", "110px"],
+    },
+    extend: {
+      fontFamily: {
+        okine: ["Okine"],
+        palanquin: ["Palanquin", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      screens: {
+        wide: "1440px",
+      },
+      colors: {
+        "off-white": "#eae5d3",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
